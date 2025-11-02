61fdec60616a698cb7510d485d8e3928cfa529f8
montse4 <160216050+montse4@users.noreply.github.com>
2025-11-02 22:08:55 +0100

    Implement Audio System
    Added pinball specific sound effects (flipper, bumper, bonus, combo, and ball lost). Implemented background music support with automatic looping. Added an independent volume control system for master, sound effects (SFX), and music.  All audio functions are ready for gameplay integration.

--- include/ModuleAudio.h
+++ include/ModuleAudio.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Module.h"
+#include "raylib.h"
 
 #define MAX_SOUNDS	16
 #define DEFAULT_MUSIC_FADE_TIME 2.0f
@@ -14,6 +15,7 @@ public:
 
 	bool Init();
 	bool CleanUp();
+	update_status Update();
 
 	// Play a music file
 	bool PlayMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME);
@@ -24,9 +26,34 @@ public:
 	// Play a previously loaded sound
 	bool PlayFx(unsigned int fx, int repeat = 0);
 
+	void PlayFlipperHit();
+	void PlayBumperHit();
+	void PlayBonusSound();
+	void PlayComboComplete();
+	void PlayBallLost();
+
+
+	void SetMasterVolume(float volume);
+	void SetSFXVolume(float volume);
+	void SetMusicVolume(float volume);
+
+	float GetMasterVolume() const { return masterVolume; }
+	float GetSFXVolume() const { return sfxVolume; }
+	float GetMusicVolume() const { return musicVolume; }
 private:
 
 	Music music;
 	Sound fx[MAX_SOUNDS];
     unsigned int fx_count;
+
+	unsigned int flipperHitFx;
+	unsigned int bumperHitFx;
+	unsigned int ballLostFx;
+	unsigned int bonusFx;
+	unsigned int comboCompleteFx;
+
+	float masterVolume;
+	float sfxVolume;
+	float musicVolume;
+
 };
--- src/ModuleAudio.cpp
+++ src/ModuleAudio.cpp
@@ -10,6 +10,16 @@ ModuleAudio::ModuleAudio(Application* app, bool start_enabled) : Module(app, sta
 {
 	fx_count = 0;
 	music = Music{ 0 };
+
+	flipperHitFx = 0;
+	bumperHitFx = 0;
+	ballLostFx = 0;
+	bonusFx = 0;
+	comboCompleteFx = 0;
+
+	masterVolume = 1.0f;
+	sfxVolume = 1.0f;
+	musicVolume = 0.5f;
 }
 
 // Destructor
@@ -26,7 +36,26 @@ bool ModuleAudio::Init()
 
     InitAudioDevice();
 
+	// Load pinball SFX
+	flipperHitFx = LoadFx("assets/audio/flipper_hit.wav");
+	bumperHitFx = LoadFx("assets/audio/bumper_hit.wav");
+	ballLostFx = LoadFx("assets/audio/ball_lost.wav");
+	bonusFx = LoadFx("assets/audio/bonus.wav");
+	comboCompleteFx = LoadFx("assets/audio/combo_complete.wav");
+
+	PlayMusic("assets/audio/pinball_theme.wav");
+
 	return ret;
+}
+
+update_status ModuleAudio::Update()
+{
+	if (IsMusicValid(music))
+	{
+		UpdateMusicStream(music);
+	}
+
+	return UPDATE_CONTINUE;
 }
 
 // Called before quitting
@@ -60,10 +89,20 @@ bool ModuleAudio::PlayMusic(const char* path, float fade_time)
 
 	bool ret = true;
 	
-    StopMusicStream(music);
-    music = LoadMusicStream(path);
+	StopMusicStream(music);
+	music = LoadMusicStream(path);
     
-    PlayMusicStream(music);
+	if (IsMusicValid(music))
+	{
+		::SetMusicVolume(music, musicVolume * masterVolume);
+		PlayMusicStream(music);
+		LOG("Successfully playing %s", path);
+	}
+	else
+	{
+		LOG("Failed to load music: %s", path);
+		ret = false;
+	}
 
 	LOG("Successfully playing %s", path);
 
@@ -103,7 +142,72 @@ bool ModuleAudio::PlayFx(unsigned int id, int repeat)
 
 	bool ret = false;
 
-	if(id < fx_count) PlaySound(fx[id]);
+	if (id > 0 && id <= fx_count)
+	{
+		SetSoundVolume(fx[id - 1], sfxVolume * masterVolume);
+		PlaySound(fx[id - 1]);
+		ret = true;
+	}
 
 	return ret;
+}
+
+void ModuleAudio::PlayFlipperHit()
+{
+	PlayFx(flipperHitFx);
+}
+
+void ModuleAudio::PlayBumperHit()
+{
+	PlayFx(bumperHitFx);
+}
+
+void ModuleAudio::PlayBonusSound()
+{
+	PlayFx(bonusFx);
+}
+
+void ModuleAudio::PlayComboComplete()
+{
+	PlayFx(comboCompleteFx);
+}
+
+void ModuleAudio::PlayBallLost()
+{
+	PlayFx(ballLostFx);
+}
+
+void ModuleAudio::SetMasterVolume(float volume)
+{
+	if (volume < 0.0f) volume = 0.0f;
+	if (volume > 1.0f) volume = 1.0f;
+
+	masterVolume = volume;
+	SetMasterVolume(masterVolume);
+
+	if (IsMusicValid(music))
+	{
+		::SetMusicVolume(music, musicVolume * masterVolume);
+	}
+}
+
+void ModuleAudio::SetSFXVolume(float volume)
+{
+	if (volume < 0.0f) volume = 0.0f;
+	if (volume > 1.0f) volume = 1.0f;
+
+	sfxVolume = volume;
+}
+
+void ModuleAudio::SetMusicVolume(float volume)
+{
+	if (volume < 0.0f) volume = 0.0f;
+	if (volume > 1.0f) volume = 1.0f;
+
+	musicVolume = volume;
+
+	if (IsMusicValid(music))
+	{
+		::SetMusicVolume(music, musicVolume * masterVolume);
+	}
 }
